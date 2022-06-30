e15d97c3571baacc775c495702a0ff4287c92d29
Fernando Chacon <fc@Fernandos-MBP.fritz.box>
2022-06-30 17:59:01 +0200

    test viz

--- a2ei_grid_analysis.py
+++ a2ei_grid_analysis.py
@@ -3,7 +3,10 @@
 import streamlit as st
 import psycopg2
 import pandas as pd 
+import plotly.graph_objects as go
+import plotly.express as px
 from datetime import datetime, date, time , timedelta
+from plotly.subplots import make_subplots
 
 
 # Page design configutration
@@ -38,17 +41,74 @@ time_end = st.sidebar.date_input('End', today)
 
 rng=[time_start,time_end]
 
-df_mppt = pd.read_sql(f'''  
+df = pd.read_sql(f'''  
                             SELECT 
                                 time + interval '1 hour' AS time
-                                ,pv_power
-                                ,charging_output_voltage
+                                ,input_voltage
+                                ,peak_voltage
                             FROM 
-                                skgsp2_logs_mppt 
+                                skgsp2_logs 
                             WHERE 
                                 aam_id = {aam_id} AND
                                 time between \'{rng[0]}\' and \'{rng[1]}\'
                             ORDER BY time 
                         ''' , conn)
 
-df_mppt
+df
+
+#Data prep
+df = df.sort_values(by = ['time'])
+df['time'] = pd.to_datetime(df['time'])
+df.set_index(df['time'],inplace=True)
+
+#Plot 
+final = make_subplots(specs=[[{"secondary_y": True}]])
+final.add_trace(go.Scatter(x = df.index, y = df['input_voltage'], name =  'Input Voltage Inv (V)',  line_color = 'rgba(82, 82, 82, .8)'))         
+final.add_trace(go.Scatter(x = df.index, y = df['peak_voltage'], name = 'Input Voltage MCU (V)',  line_color = 'rgb(245, 237, 111)'))
+#final.add_trace(go.Scatter(x = dfnew.index, y = dfnew['battery_voltage'], name =  'Battery Voltage (V)', line_color = 'rgb(251, 117, 157)', line = dict(width = 3)), secondary_y = True)
+#final.add_trace(go.Scatter(x = dfnew.index, y = dfnew['charging_output_voltage'], name =  'Battery Voltage mppt(V)', line_color = 'rgb(29, 100, 150)', line = dict(width = 3)), secondary_y = True)
+final.update_layout(plot_bgcolor = 'white',
+                    showlegend = False,
+                    font = dict(size = 12),
+                    width = 500,
+                    height = 300,
+                    title={
+                            'text':  aam_id,
+                            'y':0.9,
+                            'x':0.5,
+                            'xanchor': 'center',
+                            'yanchor': 'top'})
+final.update_xaxes( #dtick = 14,
+                    tickformat='%d/%m',
+                    ticks = 'outside', 
+                    gridcolor = 'lightgrey', 
+                    linecolor = 'lightgrey')
+
+final.update_xaxes(title = "Hour of the Day",
+                    row = 2, col = 1)
+
+final.update_yaxes(title_text = 'Voltage (V)',
+                    secondary_y = True, 
+                    showline = True,
+                    linewidth = 1,
+                    linecolor = 'lightgrey',
+                    # color = 'rgba(2251, 117, 157, 1)',
+                    showticklabels = True,
+                    ticks = 'outside',
+                    range=[20,30]
+                    )
+
+final.update_yaxes(title_text = 'Power (W)',
+                        showgrid = True,
+                        secondary_y = False, 
+                        showline = True,
+                        linewidth = 1,
+                        linecolor = 'lightgrey',
+                        showticklabels = True,
+                        gridcolor = 'lightgrey', 
+                        ticks = 'outside',
+                        range=[0,1000])
+
+st.plotly_chart(final, use_container_width=True)
+
+#-------------------------------------------
