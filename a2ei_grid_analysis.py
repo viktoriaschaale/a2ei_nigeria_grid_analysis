1f7cb81a027f8d64ddee525aabf46e0dbcb004fb
fernandochacon92 <fernandochacon@a2ei.org>
2022-07-11 15:32:31 +0200

    merged conflicts resolved2

--- a2ei_grid_analysis.py
+++ a2ei_grid_analysis.py
@@ -61,14 +61,35 @@ df = pd.read_sql(f'''       SELECT
                             WHERE
                                 aam_id = {aam_id} AND
                                 time between \'{rng[0]}\' and \'{rng[1]}\'
-<<<<<<< HEAD
                             ORDER BY time 
                         ''' , conn)
-#df_meta=
 
-#meta data about system
-st.sidebar.header('Metadata AAM : '+aam_id)
-#st.sidebar.table(aam_main_sidebar.T)
+cust = pd.read_sql(f''' SELECT
+                           aam_id, 
+                           country, 
+                           location, 
+                           latitude, 
+                           longitude,
+                           cust_type,
+                           bat_size, 
+                           inv_size,
+                           mcu_version,
+                           installation_date, 
+                           installation_comp
+                        FROM
+                           skgs_customers
+                        WHERE
+                           aam_id = {aam_id}
+                        ''', conn)
+
+# meta data about system
+st.sidebar.text("")
+st.sidebar.text("")
+
+st.sidebar.markdown(
+    f'<p style="color:#FFFFFF;font-size: 25px;text-align:center;">Meta Data AAM <strong>{aam_id}</strong> <br> </p>', unsafe_allow_html=True)
+st.sidebar.markdown(
+    f'<p style="color:#FFFFFF;font-size: 20px;">{cust.country[0]} <br> Version {cust.mcu_version[0]} <br> Battery {cust.bat_size[0]} Ah <br> Inverter {cust.inv_size[0]} W <br > Installed on {cust.installation_date[0].strftime("%Y-%m-%d")} <br> Installed by {cust.installation_comp[0]} </p>', unsafe_allow_html=True)
 
 #### DATA PREP
 
@@ -152,100 +173,17 @@ df_grid_evt_off.index = df_grid_evt_off.index + 1
 
 
 ### VALUES
-=======
-                            ORDER BY time
-                        ''', conn)
-cust = pd.read_sql(f''' SELECT
-                           aam_id, 
-                           country, 
-                           location, 
-                           latitude, 
-                           longitude,
-                           cust_type,
-                           bat_size, 
-                           inv_size,
-                           mcu_version,
-                           installation_date, 
-                           installation_comp
-                        FROM
-                           skgs_customers
-                        WHERE
-                           aam_id = {aam_id}
-                        ''', conn)
-
-# meta data about system
-st.sidebar.text("")
-st.sidebar.text("")
-
-st.sidebar.markdown(
-    f'<p style="color:#FFFFFF;font-size: 25px;text-align:center;">Meta Data AAM <strong>{aam_id}</strong> <br> </p>', unsafe_allow_html=True)
-st.sidebar.markdown(
-    f'<p style="color:#FFFFFF;font-size: 20px;">{cust.country[0]} <br> Version {cust.mcu_version[0]} <br> Battery {cust.bat_size[0]} Ah <br> Inverter {cust.inv_size[0]} W <br > Installed on {cust.installation_date[0].strftime("%Y-%m-%d")} <br> Installed by {cust.installation_comp[0]} </p>', unsafe_allow_html=True)
-
-# Data prep
-df = df.sort_values(by=['time'])
-df['time'] = pd.to_datetime(df['time'])
-df.set_index(df['time'], inplace=True)
-
-# Create other usefull columns in df
-df_grid = pd.DataFrame(columns=['input_voltage', 'peak_voltage', 'output_voltage',
-                                'input_voltage_0tonan', 'peak_voltage_0tonan', 'output_voltage_5tonan', 'grid_avl'])
-df_grid['input_voltage'] = df['input_voltage']
-df_grid['peak_voltage'] = df['peak_voltage']
-df_grid['output_voltage'] = df['output_voltage']
-df_grid['input_voltage_0tonan'].mask(
-    df_grid['input_voltage'] > 0, df_grid['input_voltage'], inplace=True)  # takes out the 0V data points
-df_grid['peak_voltage_0tonan'].mask(
-    df_grid['peak_voltage'] > 0, df_grid['peak_voltage'], inplace=True)  # takes out the 0V data points
-df_grid['output_voltage_5tonan'].mask(
-    df_grid['output_voltage'] > 5, df_grid['output_voltage'], inplace=True)  # takes out the 5V data points
-
-df_grid['grid_avl'].mask(df['input_voltage'] > 0, 1, inplace=True)
-df_grid['grid_avl'].mask(df['input_voltage'] == 0, 0, inplace=True)
-df_grid['grid_avl'] = df_grid['grid_avl'].fillna(0)  # make all nan to 0 or BL
-
-# df_grid
-
-# resample the 5min data to 1 hour data
-hour_data = pd.DataFrame(
-    columns=['grid_avl_h', 'grid_avl_h_usb', 'load_w_h', 'pv_w_h'])
-hour_data['grid_avl_h'] = df_grid['grid_avl'].resample('H').mean()
-
-# hour_data
-
-# Grouped hourly data to display one typical day
-typ_day = pd.DataFrame(
-    columns=['avg_grid_avl', 'avg_grid_avl_usb', 'avg_load_w', 'avg_pv_w', 'avg_bat_v'])
-typ_day['avg_grid_avl'] = hour_data['grid_avl_h'].groupby(
-    hour_data.index.hour).mean()
-
-# typ_day
-
-# group also by day of year to get all the points for every hour to plot box plot
-typ_day_all = pd.DataFrame(
-    columns=['avg_grid_avl', 'avg_grid_avl_usb', 'avg_load_w', 'avg_pv_w', 'avg_bat_v'])
-typ_day_all['avg_grid_avl'] = df_grid['grid_avl'].groupby(
-    [df_grid.index.dayofyear.rename('day of year'), df_grid.index.hour.rename('hour')]).mean()
-
-# typ_day_all
->>>>>>> main
 
 # create usefull values from df
 avg_input_voltage = round((df_grid['input_voltage_0tonan'].mean()), 1)
 min_input_voltage = round((df_grid['input_voltage_0tonan'].min()), 1)
 max_input_voltage = round((df_grid['input_voltage_0tonan'].max()), 1)
 
-<<<<<<< HEAD
 grid_on_time_all=round((sum(daily_data['grid_on_time_daily'])),1)
 grid_off_time_all=round((sum(daily_data['grid_off_time_daily'])),1)
 
 aam_on_time_all=round((sum(daily_data['aam_on_time_daily'])),1)
 aam_off_time_all=round((sum(daily_data['aam_off_time_daily'])),1)
-=======
-avg_peak_voltage = round((df_grid['peak_voltage_0tonan'].mean()), 1)
-min_peak_voltage = round((df_grid['peak_voltage_0tonan'].min()), 1)
-max_peak_voltage = round((df_grid['peak_voltage_0tonan'].max()), 1)
->>>>>>> main
 
 avg_bl_duration = round((df_grid_evt_off['length_h'].mean()),1)
 max_bl_duration = round((df_grid_evt_off['length_h'].max()),1) 
@@ -328,7 +266,6 @@ with st.expander('Grid Analysis'):
     with col1:
         fig = go.Figure()
 
-<<<<<<< HEAD
         fig.add_trace(go.Scatter(x=df_grid.index, 
                                     y=df_grid['input_voltage_0tonan'], name="Grid Input Voltage",
                                     line_shape='linear',line_color='lightslategrey',line_width=1.5))            
@@ -388,77 +325,10 @@ with st.expander('Grid Analysis'):
                             
         st.plotly_chart(fig, use_container_width=True)
         #fig.write_image("grid_V_time_plot.pdf")
-        
-        
-=======
-        fig.add_trace(go.Scatter(x=df_grid.index,
-                                 y=df_grid['input_voltage_0tonan'], name="Grid Input Voltage Inverter",
-                                 line_shape='linear', line_color='lightslategrey', line_width=1.5))
-
-        fig.add_trace(go.Scatter(x=df_grid.index,
-                                 y=df_grid['peak_voltage_0tonan'], name="Grid Input Voltage MCU",
-                                 line_shape='linear', line_width=1.5,
-                                 line=dict(color='GoldenRod')))
-        fig.add_trace(go.Scatter(x=df_grid.index,
-                                 y=df_grid['output_voltage_5tonan'], name="AAM Output Voltage",
-                                 line_shape='linear', line_color='rgb(223,116,149)', line_width=1.5))
-
-        annotation_min = "Min: "+str(min_input_voltage) + " V"
-        annotation_max = "Max: "+str(max_input_voltage) + " V"
-
-        fig.update_layout(  # title='Grid Data',
-            plot_bgcolor='white',
-            xaxis=dict(
-                showline=True,
-                showgrid=True,
-                showticklabels=True,
-                linewidth=1.5,
-                ticks='outside',
-                title="Time",
-                gridcolor='lightgrey',
-                linecolor='lightgrey',
-                mirror=True,
-                tickformat='%d/%m %H:%M',  # %H:%M',
-                tickfont=dict(family="Fugue", size=12, color='Black')
-                # range=[0,110],
-            ),
-            yaxis=dict(
-                title=" Grid Voltage in V",
-                showgrid=True,
-                zeroline=True,
-                showline=True,
-                linewidth=1.5,
-                ticks='outside',
-                linecolor='lightgrey',
-                mirror=True,
-                showticklabels=True,
-                gridcolor='lightgrey',
-                tickfont=dict(family="Fugue", size=12, color='Black'),
-                range=[100, 250],
-            ),
-            font=dict(family="Fugue", size=12, color='black'),
-            autosize=False,
-            width=300, height=300,
-            margin=dict(l=15, r=15, b=15, t=15,  pad=2),
-            showlegend=False,
-            legend=dict(title="",
-                        orientation="h",
-                        yanchor="top",
-                        y=.95,
-                        xanchor="center",
-                        traceorder='normal',
-                        x=0.5,
-                        ),
-        )
->>>>>>> main
-
-        st.plotly_chart(fig, use_container_width=True)
-        # fig.write_image("grid_V_time_plot.pdf")
 
     #################
     with col2:
         fig = go.Figure()
-<<<<<<< HEAD
         
         fig.add_trace(go.Box(y=df_grid['input_voltage_0tonan'],name="input_voltage",
                             marker_color = 'lightslategrey'))
@@ -510,65 +380,9 @@ with st.expander('Grid Analysis'):
                                         x=0.95,
                                         ),
                                 )                                
-=======
-
-        fig.add_trace(go.Box(y=df_grid['input_voltage_0tonan'], name="input_voltage",
-                             marker_color='lightslategrey'))
-        fig.add_trace(go.Box(y=df_grid['peak_voltage_0tonan'], name="peak_voltage",
-                             marker_color='GoldenRod'))
-        fig.add_trace(go.Box(y=df_grid['output_voltage'], name="output_voltage",
-                             marker_color='rgb(223,116,149)'))
-
-        fig.update_layout(  # title='Grid Data',
-            plot_bgcolor='white',
-            xaxis=dict(
-                showline=True,
-                showgrid=True,
-                showticklabels=False,
-                linewidth=1.5,
-                ticks='outside',
-                title="",
-                gridcolor='lightgrey',
-                linecolor='lightgrey',
-                mirror=True,
-                tickformat='%d/%m %H:%M',  # %H:%M',
-                tickfont=dict(family="Fugue", size=12, color='Black')
-                # range=[0,110],
-            ),
-            yaxis=dict(
-                title=" Grid Voltage in V",
-                showgrid=True,
-                zeroline=True,
-                showline=True,
-                linewidth=1.5,
-                ticks='outside',
-                linecolor='lightgrey',
-                mirror=True,
-                showticklabels=True,
-                gridcolor='lightgrey',
-                tickfont=dict(family="Fugue", size=12, color='Black'),
-                range=[100, 250],
-            ),
-            font=dict(family="Fugue", size=12, color='black'),
-            autosize=False,
-            width=300, height=300,
-            margin=dict(l=15, r=15, b=15, t=15,  pad=2),
-            showlegend=True,
-            legend=dict(title="",
-                        orientation="v",
-                        yanchor="bottom",
-                        y=0.05,
-                        xanchor="right",
-                        traceorder='normal',
-                        x=0.95,
-                        ),
-        )
->>>>>>> main
         st.plotly_chart(fig, use_container_width=True)
         # fig.write_image("grid_V_box_plot.pdf")
     #########################################
-<<<<<<< HEAD
-    
 
     st.info('avg_input_voltage: '+str( avg_input_voltage))
     st.info('min_input_voltage: '+str( min_input_voltage))
@@ -615,21 +429,7 @@ with st.expander('Grid Analysis'):
                             )
         st.plotly_chart(fig,use_container_width=True)
         #fig.write_image("AAM_availability.pdf")
-=======
 
-    col1, col2 = st.columns([1, 1])
-    with col1:
-        st.info('avg_input_voltage: '+str(avg_input_voltage))
-        st.info('min_input_voltage: '+str(min_input_voltage))
-        st.info('max_input_voltage: '+str(max_input_voltage))
-
-    with col2:
-        st.info('avg_peak_voltage: '+str(avg_peak_voltage))
-        st.info('min_peak_voltage: '+str(min_peak_voltage))
-        st.info('max_peak_voltage: '+str(max_peak_voltage))
-
-    ##########################################
->>>>>>> main
     ################################
 
     st.markdown("<h3 style='text-align: center'>Hourly Grid Availability Profile</h3>",
@@ -701,7 +501,6 @@ with st.expander('Grid Analysis'):
     # fig.write_image("hour_profile.pdf")
 
 ################################
-<<<<<<< HEAD
         
     st.markdown("<h3 style='text-align: center'>Power Outage Duration</h3>", unsafe_allow_html=True)
 
@@ -1151,5 +950,3 @@ with st.expander('Grid Analysis'):
                                 )                                
         st.plotly_chart(fig, use_container_width=True)
         #fig.write_image("grid_V_box_plot.pdf")
-=======
->>>>>>> main
