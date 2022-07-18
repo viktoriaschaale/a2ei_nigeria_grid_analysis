361cc60f82ca279bff771064211bab8b547f48d1
viktoriaschaale <viktoria.schaale@a2ei.org>
2022-07-18 17:36:04 +0200

    added two separate files: add.py & helpers.py

--- app.py
+++ app.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# cd Documents/git/a2ei_nigeria_grid_analysis/ ; streamlit run app.py
+
+from datetime import datetime
 import streamlit as st
-# from helpers import ...
+from helpers import fetch_cust, fetch_df, fetch_data_density
+
+
+st.set_page_config(
+    page_title="A2EI Grid Analysis",
+    page_icon=":electric_plug:",
+    initial_sidebar_state="expanded",
+    layout="wide",
+)
+
+# SIDEBAR
+st.sidebar.image("images/A2EI_horizontal_white_RGB.png",
+                 use_column_width=True)
+st.sidebar.text("")
+st.sidebar.text("")
+
+cust = fetch_cust()
+
+cust
+
+aam_id = st.sidebar.selectbox('AAM ID', cust.aam_id)
+time_start = st.sidebar.date_input('Start', value=(datetime(2022, 3, 1)))
+today = datetime.today()
+time_end = st.sidebar.date_input('End', today)
+rng = [time_start, time_end]
+
+df = fetch_df(aam_id, rng)
+df
+
+data_density = fetch_data_density(aam_id)
+data_density
--- helpers.py
+++ helpers.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from sqlalchemy.engine import create_engine
-from datetime import datetime
-import numpy as np
+from sqlalchemy import create_engine
+# from datetime import datetime
+# import numpy as np
 import pandas as pd
-from plotly_calplot import calplot
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+# from plotly_calplot import calplot
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
 import streamlit as st
+
+
+db = st.secrets["aws"]
+
+
+@st.cache(allow_output_mutation=True)
+def get_db_conn():
+    engine = create_engine(
+        f"{db['lang']}://{db['user']}:{db['password']}@{db['host']}/{db['dbname']}")
+    return engine.connect()
+
+
+@st.cache
+def fetch_cust():
+    with st.spinner('Loading Data...'):
+        sql = '''SELECT
+                                    aam_id,
+                                    country,
+                                    location,
+                                    latitude,
+                                    longitude,
+                                    cust_type,
+                                    bat_size,
+                                    inv_size,
+                                    mcu_version,
+                                    installation_date,
+                                    installation_comp
+                                FROM
+                                    skgs_customers'''
+        cust = pd.read_sql(sql, con=get_db_conn())
+    return cust
 
 
 @st.cache
@@ -16,3 +47,37 @@ def data_prep(df):
     df['time'] = pd.to_datetime(df['time'])
     df.set_index(df['time'], inplace=True)
     return df
+
+
+@st.cache
+def fetch_df(aam_id, rng):
+    df = pd.read_sql(f'''       SELECT
+                                    time + interval '1 hour' AS time
+                                    ,input_voltage
+                                    ,peak_voltage
+                                    ,output_voltage
+                                FROM
+                                    skgsp2_logs
+                                WHERE
+                                    aam_id = {aam_id} AND
+                                    time between \'{rng[0]}\' and \'{rng[1]}\'
+                                ORDER BY time
+                            ''', get_db_conn())
+    return df
+
+
+@st.cache
+def fetch_data_density(aam_id):
+    data_density = pd.read_sql(f'''       SELECT
+                                    time + interval '1 hour' AS time
+                                    ,input_voltage
+                                    ,peak_voltage
+                                FROM
+                                    skgsp2_logs
+                                WHERE
+                                    aam_id = {aam_id} AND
+                                    time between '2021-01-01' and '2022-12-01'
+                                ORDER BY time
+                            ''', get_db_conn())
+    data_density = data_prep(data_density)
+    return data_density
